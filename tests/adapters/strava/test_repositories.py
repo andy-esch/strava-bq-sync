317133a10cc356ad1130e633ee5bf4012d2d7bca
Andy Eschbacher <andyesch@protonmail.com>
2025-07-23 12:22:07 -0400

    test: adds missing coverage for failures

--- tests/adapters/strava/test_repositories.py
+++ tests/adapters/strava/test_repositories.py
@@ -9,7 +9,11 @@ from stravabqsync.adapters.strava._repositories import (
 )
 from stravabqsync.config import StravaApiConfig
 from stravabqsync.domain import StravaActivity, StravaTokenSet
-from stravabqsync.exceptions import ActivityNotFoundError, StravaTokenError
+from stravabqsync.exceptions import (
+    ActivityNotFoundError,
+    StravaApiError,
+    StravaTokenError,
+)
 
 
 @pytest.fixture
@@ -56,6 +60,15 @@ class TestStravaTokenRepo:
             with pytest.raises(StravaTokenError):
                 token_repo.refresh()
 
+    def test_failed_request_non_401(self, token_repo):
+        with Mocker() as m:
+            m.post(
+                token_repo._api_config.token_url, status_code=500, text="Server Error"
+            )
+
+            with pytest.raises(StravaApiError):
+                token_repo.refresh()
+
 
 class TestStravaActivitiesRepo:
     def test_read_activity_by_id(self, activities_repo, activity_json):
@@ -88,4 +101,24 @@ class TestStravaActivitiesRepo:
             )
             m.get(endpoint, status_code=404)
             with pytest.raises(ActivityNotFoundError):
+                _ = activities_repo.read_activity_by_id(activity_id)
+
+    def test_read_activity_token_expired(self, activities_repo):
+        activity_id = 12345678987654321
+        with Mocker() as m:
+            endpoint = (
+                f"{activities_repo._api_config.api_base_url}/activities/{activity_id}"
+            )
+            m.get(endpoint, status_code=401)
+            with pytest.raises(StravaTokenError):
+                _ = activities_repo.read_activity_by_id(activity_id)
+
+    def test_read_activity_api_error(self, activities_repo):
+        activity_id = 12345678987654321
+        with Mocker() as m:
+            endpoint = (
+                f"{activities_repo._api_config.api_base_url}/activities/{activity_id}"
+            )
+            m.get(endpoint, status_code=500, text="Server Error")
+            with pytest.raises(StravaApiError):
                 _ = activities_repo.read_activity_by_id(activity_id)
