64ff6d7d363153ef52355cd6a1d8394f3350757e
Andy Eschbacher <andyesch@protonmail.com>
2025-07-23 09:23:57 -0400

    test: adjust tests for app config

--- tests/adapters/strava/test_repositories.py
+++ tests/adapters/strava/test_repositories.py
@@ -7,6 +7,7 @@ from stravabqsync.adapters.strava._repositories import (
     StravaActivitiesRepo,
     StravaTokenRepo,
 )
+from stravabqsync.config import StravaApiConfig
 from stravabqsync.domain import StravaActivity, StravaTokenSet
 from stravabqsync.exceptions import ActivityNotFoundError, StravaTokenError
 
@@ -19,6 +20,11 @@ def tokenset():
 
 
 @pytest.fixture
+def api_config():
+    return StravaApiConfig()
+
+
+@pytest.fixture
 def activity_json():
     with open("tests/fixtures/activity_1.json", "r", encoding="utf-8") as fin:
         activity_json = json.load(fin)
@@ -27,25 +33,25 @@ def activity_json():
 
 
 @pytest.fixture
-def token_repo(tokenset):
-    return StravaTokenRepo(tokens=tokenset)
+def token_repo(tokenset, api_config):
+    return StravaTokenRepo(tokens=tokenset, api_config=api_config)
 
 
 @pytest.fixture
-def activities_repo(tokenset):
-    return StravaActivitiesRepo(tokenset._replace(access_token="baz"))
+def activities_repo(tokenset, api_config):
+    return StravaActivitiesRepo(tokenset._replace(access_token="baz"), api_config)
 
 
 class TestStravaTokenRepo:
     def test_refresh(self, token_repo):
         with Mocker() as m:
-            m.post(token_repo._url, json={"access_token": "baz"})
+            m.post(token_repo._api_config.token_url, json={"access_token": "baz"})
             expected = token_repo.refresh()
             assert expected.access_token == "baz"
 
     def test_failed_request(self, token_repo):
         with Mocker() as m:
-            m.post(token_repo._url, status_code=401)
+            m.post(token_repo._api_config.token_url, status_code=401)
 
             with pytest.raises(StravaTokenError):
                 token_repo.refresh()
@@ -55,10 +61,10 @@ class TestStravaActivitiesRepo:
     def test_read_activity_by_id(self, activities_repo, activity_json):
         activity_id = 12345678987654321
         with Mocker() as m:
-            m.get(
-                activities_repo._activity_endpoint.format(activity_id=activity_id),
-                json=activity_json,
+            endpoint = (
+                f"{activities_repo._api_config.api_base_url}/activities/{activity_id}"
             )
+            m.get(endpoint, json=activity_json)
             resp = activities_repo.read_activity_by_id(activity_id)
 
         assert resp.id == activity_id
@@ -66,10 +72,10 @@ class TestStravaActivitiesRepo:
     def test_read_activity_by_id_type(self, activities_repo, activity_json):
         activity_id = 12345678987654321
         with Mocker() as m:
-            m.get(
-                activities_repo._activity_endpoint.format(activity_id=activity_id),
-                json=activity_json,
+            endpoint = (
+                f"{activities_repo._api_config.api_base_url}/activities/{activity_id}"
             )
+            m.get(endpoint, json=activity_json)
             resp = activities_repo.read_activity_by_id(activity_id)
 
         assert isinstance(resp, StravaActivity)
@@ -77,9 +83,9 @@ class TestStravaActivitiesRepo:
     def test_read_activity_not_found(self, activities_repo, activity_json):
         activity_id = -10
         with Mocker() as m:
-            m.get(
-                activities_repo._activity_endpoint.format(activity_id=activity_id),
-                status_code=404,
+            endpoint = (
+                f"{activities_repo._api_config.api_base_url}/activities/{activity_id}"
             )
+            m.get(endpoint, status_code=404)
             with pytest.raises(ActivityNotFoundError):
                 _ = activities_repo.read_activity_by_id(activity_id)
